86e72f4d675bbbb8d2038ca7804b9b7b9cb5ea38
mohauMakunyane22 <2664333@students.wits.ac.za>
2025-08-06 21:06:13 +0200

    changed file structure in docs for clarity

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -6,8 +6,8 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+			title: 'Live Sport Stats Documentation',
+			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/mohauMakunyane22/LiveSportUpdates' }],
 			sidebar: [
 				{
 					label: 'Guides',
